0da1a91666f6595efad08f151047f9a9f70652bb
Chris Haen <chaen@foundrydigital.com>
2022-04-06 18:44:22 -0400

    Mainnet 17

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 🌊 Flow DPS Docker
 
-🔨 `DOCKER_BUILDKIT=1 docker build -t flow-mainnet16 .` 🔨  
+🔨 `DOCKER_BUILDKIT=1 docker build -t flow-mainnet17 .` 🔨  
   
-🏃 `docker run --mount type=bind,source=$PWD/data,target=/data -m 275g -p 8080:8080 flow-mainnet16` 🏃  
+🏃 `docker run --mount type=bind,source=$PWD/data,target=/data -m 275g -p 8080:8080 flow-mainnet17` 🏃  
--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ set -e
 
 source common.sh
 
-DATA_DIR="/data/mainnet-16"
+DATA_DIR="/data/mainnet-17"
 GCP_BUCKET=""
 SEED_ADDRESS=""
 SEED_KEY=""
 
-bootstrap "mainnet" 16 "$DATA_DIR"
+bootstrap "mainnet" 17 "$DATA_DIR"
 
 export DATA_DIR="$DATA_DIR"
 export GCP_BUCKET="$GCP_BUCKET"
-export SEED_ADDRESS="access-007.mainnet16.nodes.onflow.org:3570" # "$SEED_ADDRESS" sporks.json is wrong currently
-export SEED_KEY="28a0d9edd0de3f15866dfe4aea1560c4504fe313fc6ca3f63a63e4f98d0e295144692a58ebe7f7894349198613f65b2d960abf99ec2625e247b1c78ba5bf2eae" # "$SEED_ADDRESS" sporks.json is wrong currently
+export SEED_ADDRESS="access-007.mainnet17.nodes.onflow.org:3570" # "$SEED_ADDRESS" sporks.json is wrong currently
+export SEED_KEY="28a0d9edd0de3f15866dfe4aea1560c4504fe313fc6ca3f63a63e4f98d0e295144692a58ebe7f7894349198613f65b2d960abf99ec2625e247b1c78ba5bf2eae" # "$SEED_KEY" sporks.json is wrong currently
 
 /usr/bin/supervisord -c /supervisord.conf
